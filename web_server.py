06f111b3d842ddfd567ea98c556cc9e88a931a63
Yoav Shamay <yoavsh97@gmail.com>
2025-02-24 15:09:29 +0200

    done some changes

--- web_server.py
+++ web_server.py
@@ -1,9 +1,28 @@
 import socket
 import os
-class BadRequest(Exception):
-    pass
 
-def retrieve_from_input_dictionary(dictionary, key, error_message):
+
+CONTENT_TYPE_BY_EXTENSION = {"html":"text/html", "css": "text/css", "js": "application/javascript", "jpg": "image/jpeg",
+                          "gif": "image/gif", "png": "image/png", "ico": "image/x-icon"}
+PLAINTEXT_CONTENT_TYPE = "text/plain"
+CONTENT_TYPE_HEADER_NAME = "Content-Type" #TODO needs to be a constant?
+ROOT_DIRECTORY = "webroot"
+UPLOADS_PATH = "/imgs" #TODO change to saved_images? maybe do it differnt than ROOT?
+#TODO \r\n as constnat?
+HTTP_VERSION = "HTTP/1.1"
+
+OK_STATUS_CODE = 200
+BAD_REQUEST_STATUS_CODE = 400
+NOT_FOUND_STATUS_CODE = 404
+INTERNAL_SERVER_ERROR_STATUS_CODE = 500
+#TODO status messages as constants
+
+class BadRequest(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+def retrieve_from_input_dictionary(dictionary, key, error_message): #TODO do I need the message, make it generic somehow?
     if key not in dictionary:
         raise BadRequest(error_message)
     return dictionary[key]
@@ -14,9 +33,10 @@ def trim_linear_whitespaces(string):
     '''
     return string.strip(" \t")
 
+HEADER_VALUE_SEPERATOR = ':' #TODO needs to be a constant?
 
 def parse_header(header_line):
-    header_splitter = header_line.find(':')
+    header_splitter = header_line.find(HEADER_VALUE_SEPERATOR)
     if header_splitter == -1:
         raise BadRequest("Missing colon in header")
     header_value = trim_linear_whitespaces(header_line[header_splitter + 2:])
@@ -88,7 +108,7 @@ class ClientConnection:
 
     def recieve_line(self):
         cur_text = ""
-        while cur_text[-2:] != "\r\n": #TODO \r\n constant
+        while cur_text[-2:] != "\r\n":
             cur_text += self.__socket.recv(1).decode()
         return cur_text[:-2]
 
@@ -109,14 +129,14 @@ class ClientConnection:
         #receive content
         body = None
         if "Content-Length" in headers:
-            length = int(headers["Content-Length"][0])
+            length = int(headers["Content-Length"])
             body = self.__socket.recv(length)
         return HttpRequest(method, actual_path, query_parameters, headers, body)
 
 
     def send_response(self, response):
         #send first line
-        first_line = f"HTTP/1.1 {response.get_status_code()} {response.get_status_message()}\r\n" #TODO HTTP/1.1 as constant
+        first_line = f"{HTTP_VERSION} {response.get_status_code()} {response.get_status_message()}\r\n" #TODO HTTP/1.1 as constant
         self.__socket.send(first_line.encode())
         headers = response.get_headers()
         for header, value in headers.items():
@@ -130,9 +150,9 @@ class ClientConnection:
         self.__socket.close()
 
 class HttpServer: #TODO is this class neccessary?
-    def __init__(self):
+    def __init__(self, host, port):
         self.__server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.__server_socket.bind(("localhost", 80)) #TODO make port a constant? make it a parameter? also localhost a constnat
+        self.__server_socket.bind((host, port))
         self.__server_socket.listen()
     def accept_client(self):
         client_socket, _ = self.__server_socket.accept()
@@ -150,44 +170,48 @@ def write_to_file(file_path, content):
     file.write(content)
     file.close()
 
-def parse_header_value_parameters(header_value_str): #TODO seperators as constant?
-    value_parts = header_value_str.split(';')
+PARAMETERS_SEPERATOR = ';'
+PARAMETER_NAME_VALUE_SEPEATOR = '='
+
+def parse_header_value_parameters(header_value_str): #TODO handle quoted values
+    value_parts = header_value_str.split(PARAMETERS_SEPERATOR)
     main_value = trim_linear_whitespaces(value_parts[0])
     parameters = {}
     for parameter_str in value_parts[1:]:
         parameter_str = trim_linear_whitespaces(parameter_str)
-        name_value_splitter = parameter_str.find('=')
+        name_value_splitter = parameter_str.find(PARAMETER_NAME_VALUE_SEPEATOR)
+        if name_value_splitter == -1:
+            raise BadRequest("Invalid header parameter syntax")
         name = parameter_str[:name_value_splitter]
         value = parameter_str[name_value_splitter + 1:]
         parameters[name] = value
     return main_value, parameters
 
 def parse_form_data(request_body, request_headers):
-    content_type_header = retrieve_from_input_dictionary(request_headers, "Content-Type", "Missing Content-Type header")
+    content_type_header = retrieve_from_input_dictionary(request_headers, CONTENT_TYPE_HEADER_NAME, "Missing Content-Type header")
     _, content_type_params = parse_header_value_parameters(content_type_header)
-    boundary = retrieve_from_input_dictionary(content_type_params, "boundary", "Missing boundary in Content-Type")
+    boundary = retrieve_from_input_dictionary(content_type_params, "boundary", "Missing boundary in Content-Type header")
     #remove first and last boundary
-    request_body = request_body.removeprefix(f"{boundary}\r\n".encode()).removesuffix(f"\r\n{boundary}\r\n".encode())
+    print(boundary)
+    prefix = f"--{boundary}\r\n"
+    suffix = f"\r\n--{boundary}--\r\n"
+    request_body = request_body.removeprefix(prefix.encode()).removesuffix(suffix.encode())
     #seperate headers and body
-    headers_seperator = request_body.find(b"\r\n\r\n")
-    if headers_seperator == -1:
-        raise BadRequest("Invalid body structure") #TODO message?
-    headers_str = request_body[:headers_seperator]
-    content = request_body[headers_seperator + 4:]
+    print(request_body)
+    headers_body_seperator = request_body.find(b"\r\n\r\n")
+    if headers_body_seperator == -1:
+        raise BadRequest("Invalid body structure")
+    headers_str = request_body[:headers_body_seperator]
+    content = request_body[headers_body_seperator + 4:]
     #parse headers
     headers_splitted = headers_str.split(b"\r\n")
     headers = {}
     for header_str in headers_splitted:
-        header, value = parse_header(header_str)
+        header, value = parse_header(header_str.decode())
         headers[header] = value
     return headers, content
 
 
-CONTENT_TYPE_BY_EXTENSION = {"html":"text/html", "css": "text/css", "js": "application/javascript", "jpg": "image/jpeg",
-                          "gif": "image/gif", "png": "image/png", "ico": "image/x-icon"}
-PLAINTEXT_CONTENT_TYPE = "text/plain"
-ROOT_DIRECTORY = "webroot"
-UPLOADS_PATH = "/imgs" #TODO change to saved_images? maybe do it differnt than ROOT?
 
 def calculate_next(request):
     parameters = request.get_query_parameters()
@@ -197,8 +221,8 @@ def calculate_next(request):
     else:
         result = 5 #TODO should I BadRequest()? otherwise make constant?
     response_body = str(result).encode()
-    response_headers = {"Content-Type": PLAINTEXT_CONTENT_TYPE} #TODO constants
-    return HttpResponse(200, "OK", response_headers, response_body)
+    response_headers = {CONTENT_TYPE_HEADER_NAME: PLAINTEXT_CONTENT_TYPE} #TODO constants
+    return HttpResponse(OK_STATUS_CODE, "OK", response_headers, response_body)
 
 def calculate_area(request):
     parameters = request.get_query_parameters()
@@ -206,20 +230,29 @@ def calculate_area(request):
     width = int(retrieve_from_input_dictionary(parameters, "width", "Missing width"))
     area = (height * width) / 2
     response_body = str(area).encode()
-    response_headers = {"Content-Type": PLAINTEXT_CONTENT_TYPE} #TODO constants
-    return HttpResponse(200, "OK", response_headers, response_body)
+    response_headers = {CONTENT_TYPE_HEADER_NAME: PLAINTEXT_CONTENT_TYPE}
+    return HttpResponse(OK_STATUS_CODE, "OK", response_headers, response_body)
 
-
+FILENAME_FORBIDDEN_CHARACTERS = "/\\?*:|\"<>"
+FORBIDDEN_LAST_FILENAME_CHARACTERS = ". "
+def is_valid_filename(name): #TODO add more checks?
+    if any(char in FILENAME_FORBIDDEN_CHARACTERS for char in name):
+        return False
+    if name[-1] in FORBIDDEN_LAST_FILENAME_CHARACTERS:
+        return False
+    return True
+    
 def upload(request : HttpRequest):
     #upload image from form-data
     form_data_headers, form_data_content = parse_form_data(request.get_body(), request.get_headers())
     content_disposition_header = retrieve_from_input_dictionary(form_data_headers, "Content-Disposition", "Missing Content-Disposition header in request body")
     _, content_disposition_params = parse_header_value_parameters(content_disposition_header)
     file_name = retrieve_from_input_dictionary(content_disposition_params, "filename", "Missing filename in Content-Disposition header in request body")
-    #TODO file name validation
-    file_path = ROOT_DIRECTORY + UPLOADS_PATH + "/" + file_name #TODO include / in UPLOADS_PATH?
+    if not is_valid_filename(file_name):
+        raise BadRequest("Invalid filename")
+    file_path = ROOT_DIRECTORY + UPLOADS_PATH + "/" + file_name
     write_to_file(file_path, form_data_content)
-    return HttpResponse(200, "OK", {}, b"") #TODO add success message?
+    return HttpResponse(OK_STATUS_CODE, "OK", {}, b"") #TODO add success message? switch to no response status code?
 
 def get_image(request):
     #parameters: image-name
@@ -234,11 +267,11 @@ def get_image(request):
             content_type = CONTENT_TYPE_BY_EXTENSION[extension]
         else:
             content_type = PLAINTEXT_CONTENT_TYPE
-        headers = {"Content-Type": content_type}
-        return HttpResponse(200, "OK", headers, image_content)
+        headers = {CONTENT_TYPE_HEADER_NAME: content_type}
+        return HttpResponse(OK_STATUS_CODE, "OK", headers, image_content)
     else:
-        print("404") #TODO implement
-        return None
+        headers = {}
+        return HttpResponse(NOT_FOUND_STATUS_CODE, "Not Found", headers, b"") #TODO add message?
 
 def read_file(request_path):
     file_path = ROOT_DIRECTORY + request_path
@@ -246,12 +279,11 @@ def read_file(request_path):
     if os.path.exists(file_path): #TODO make sure no path traversal? http://stackoverflow.com/questions/45188708/how-to-prevent-directory-traversal-attack-from-python-code
         response_body = get_file_content(file_path)
         file_extension = file_path.split('.')[-1] #TODO check for existance
-        response_headers = {"Content-Type": CONTENT_TYPE_BY_EXTENSION[file_extension]} #TODO check extension existance
-        return HttpResponse(200, "OK", response_headers, response_body) #TODO constants for 200 and OK
+        response_headers = {CONTENT_TYPE_HEADER_NAME: CONTENT_TYPE_BY_EXTENSION[file_extension]} #TODO check extension existance
+        return HttpResponse(OK_STATUS_CODE, "OK", response_headers, response_body) #TODO constants for 200 and OK
     else:
-        #TODO send 404
-        print("404")
-        pass
+        headers = {}
+        return HttpResponse(NOT_FOUND_STATUS_CODE, "Not Found", headers, b"") #TODO add message?
 
 API_METHODS = {
     ("GET", "/calculate-next"): calculate_next,
@@ -261,7 +293,7 @@ API_METHODS = {
 }
 
 def main():
-    server = HttpServer()
+    server = HttpServer("localhost", 80)
     while True:
         conn = server.accept_client() #Handle multiple requests
         try:
@@ -274,14 +306,15 @@ def main():
                 response = read_file(request_path)
             conn.send_response(response)
         except BadRequest as e:
-            #TODO support extra bad request info?
-            #TODO send bad request
-            print(e)
-            pass
+            body = e.message.encode()
+            headers = {CONTENT_TYPE_HEADER_NAME: PLAINTEXT_CONTENT_TYPE}
+            response = HttpResponse(BAD_REQUEST_STATUS_CODE, "Bad Request", headers, body)
+            conn.send_response(response)
         except Exception as e:
-            #TODO send internal server error
             print(e)
-            pass
+            headers = {}
+            response = HttpResponse(INTERNAL_SERVER_ERROR_STATUS_CODE, "Internal Server Error", headers, b"")
+            conn.send_response(response)
         conn.close()
 
 
@@ -289,18 +322,16 @@ if __name__ == "__main__":
     main()
 
 '''
-TODO 4.10, 4.11
 TODO status codes as contants
 TODO If string concat is quadratic move to list
 TODO Add timeout
-TODO Check permission to read from file
+TODO Check permission to read from file?
 TODO Prevent directory traversal attack
 TODO I need to handle invalid request format?
 TODO Redirect to index.html by default?
 TODO read 4.4 for extra guidelines
 TODO handle url encoding of characters
 TODO percent encoding in form-data
-TODO error messages
 TODO header name cant contain percent encoding but header value can
 TODO parameter value encoding https://datatracker.ietf.org/doc/html/rfc5987
 '''
